1cf1e429c8eb460009a68deca25f51f4f4b43d19
joseperez8715 <joseperez8715@gmail.com>
2023-03-30 16:03:43 -0400

    Update PruebaPyqt.py

--- PruebaPyqt.py
+++ PruebaPyqt.py
@@ -1,19 +1,16 @@
-#from PyQt5 import QtWidgets
-#from PyQt5.QtWidgets import QApplication, QMainWindow
-#import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
-#def window():
-    #app = QApplication(sys.argv)
-    #win = QMainWindow()
-    #win.setGeometry(200, 200, 300, 300)
-    #win.setWindowTitle("Titulo")
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
+    win.setWindowTitle("Titulo")
     
-    #win.show
-    #sys.exit(app.exec_())
+    win.show
+    sys.exit(app.exec_())
     
-#window()
+window()
 
 
-#Simple comentario luego de sincronisar github.
-#Otro cambio realizado a las 2:50 PM
-#3cer comentario
