de1500cb1741d7baee79c1e3a5d829b62190ab38
joseperez8715 <joseperez8715@gmail.com>
2023-04-16 15:07:04 -0400

    JP

--- MiniNomina/FrmDatos.py
+++ MiniNomina/FrmDatos.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QPushButton, QDialog, QWidget, QTableView, QTableWidget, QHeaderView, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtGui import QPainter, QPageLayout, QPageSize
+from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 from Conexion_db import conectar_db
 from Consultas_db import mostrar_datos_de_faltantes, mostrar_datos_de_empleados
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel, QSqlQuery
@@ -27,13 +29,13 @@ class VentanaDatos(QMainWindow):
         #------------------------------------------------------------------------------------------------------
         # Llama a la funcion que cierra la ventana
         self.BtnSalir.clicked.connect(self.fn_Salir)
-        #self.BtnImprimir.clicked.connect(self.datos_de_select)
+        self.BtnImprimir.clicked.connect(self.imprimir_datos_tbtabla)
         
         
     #------------------------------------------------------------------------------------------------------
     #------------------------------------------------------------------------------------------------------
     # Muestra los datos de la consulta contenida en mostrar_datos_de_faltantes del modulo Consultas_db    
-    def datos_en_tabla_faltantes(self):    
+    def datos_en_tabla_faltantes(self):
         mostrar_datos_de_faltantes(self.tbtabla)
         
     #------------------------------------------------------------------------------------------------------
@@ -51,6 +53,41 @@ class VentanaDatos(QMainWindow):
     
     #------------------------------------------------------------------------------------------------------
     #------------------------------------------------------------------------------------------------------
+    def imprimir_datos_tbtabla(self):
+        # Crear una instancia de QPrinter
+        printer = QPrinter(QPrinter.HighResolution)
+
+        # Crear un objeto QPrintDialog y mostrarlo al usuario
+        print_dialog = QPrintDialog(printer)
+        if print_dialog.exec_() == QPrintDialog.Accepted:
+            # Configurar el tamaño del QTableView para que quepa en una página
+            self.tbtabla.resizeColumnsToContents()
+            self.tbtabla.setFixedHeight(self.tbtabla.rowHeight(0) * self.tbtabla.model().rowCount() + 20)
+
+            # Configurar la página para que se ajuste al tamaño del QTableView
+            page_size = printer.pageLayout().pageSize()
+            if page_size == QPageSize.Custom:
+                page_size = printer.pageRect().size()
+            layout = QPageLayout(QPageSize(page_size), printer.pageLayout().orientation(),
+printer.pageLayout().margins(), printer.pageLayout().minimumMargins(), printer.pageLayout().units())
+            printer.setPageLayout(layout)
+
+            # Crear un objeto QPainter y dibujar el contenido del QTableView en el objeto QPrinter
+            painter = QPainter()
+            painter.begin(printer)
+            self.tbtabla.render(painter)
+            painter.end()
+
+            # Llamar a la función newPage() para comenzar a imprimir en una nueva página si es necesario
+            if printer.pageOrder() == QPrinter.LastPageFirst:
+                printer.setPageOrder(QPrinter.FirstPageFirst)
+            if len(printer.supportedPageSizes()) > 1:
+                printer.newPage()
+        
+        
+        
+    #------------------------------------------------------------------------------------------------------
+    #------------------------------------------------------------------------------------------------------    
     # Funion para cerar la ventana llamado desde el boton Salir.    
     def fn_Salir(self):
         self.close()
