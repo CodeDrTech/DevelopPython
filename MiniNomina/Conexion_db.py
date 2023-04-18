ef476e972f5a48e1c6395a80214dc12d391c21f3
CodeDrTech <joseperez8715@gmail.com>
2023-04-18 08:50:01 -0400

    JP

--- MiniNomina/Conexion_db.py
+++ MiniNomina/Conexion_db.py
@@ -1,11 +1,11 @@
 import sqlite3
-from PyQt5.QtSql import QSqlQuery, QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QTableView, QMessageBox
+from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtWidgets import QMessageBox
 
 def ruta_database():
     Ruta = 'C:\\Users\\Jose\\Documents\\GitHub\\DevelopPython\\Base de datos\\MiniNomina.db'
     Ruta2 = 'C:\\Users\\acer\\OneDrive\\Documentos\\GitHub\\DevelopPython\\Base de datos\\MiniNomina.db'
-    return Ruta
+    return Ruta2
 
 def conectar_db():
     conn = sqlite3.connect(ruta_database())
