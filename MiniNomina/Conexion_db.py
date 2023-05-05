fed274413a1260df5edd90522f068e28c0a24987
CodeDrTech <joseperez8715@gmail.com>
2023-05-05 16:33:20 -0400

    JP

--- MiniNomina/Conexion_db.py
+++ MiniNomina/Conexion_db.py
@@ -22,11 +22,19 @@ from PyQt5.QtWidgets import QApplication
 
 def funcion_de_conexion():
     ruta_db = "ruta/a/la/base/de/datos.db"
-    ruta_configuracion = "ruta/a/archivo/de/configuracion.txt"
+    ruta_configuracion = "MiniNomina/configuracion.txt"
     
+    # Crear archivo de configuración si no existe
     if not os.path.exists(ruta_configuracion):
-        # La configuración no existe, se debe crear
-        # y pedirle al usuario que seleccione la ruta de la base de datos
+        with open(ruta_configuracion, "w") as f:
+            f.write("")  # Escribir un archivo vacío
+    
+    with open(ruta_configuracion, "r") as f:
+        ruta_guardada = f.read().strip()
+    
+    if not ruta_guardada:
+        # La configuración no tiene una ruta de base de datos
+        # Se debe pedirle al usuario que seleccione la ruta
         ruta_seleccionada, _ = QFileDialog.getOpenFileName(None, "Seleccionar base de datos", "", "Archivos de base de datos (*.db)")
         
         # Se guarda la ruta seleccionada en el archivo de configuración
@@ -35,30 +43,24 @@ def funcion_de_conexion():
         
         # Código para crear la base de datos
         
+    elif ruta_guardada != ruta_db:
+        # La ruta de la base de datos ha cambiado
+        # Se debe mostrar un mensaje de error o hacer algo al respecto
+        
+        # Se pide al usuario que seleccione la nueva ruta de la base de datos
+        ruta_seleccionada, _ = QFileDialog.getOpenFileName(None, "Seleccionar base de datos", "", "Archivos de base de datos (*.db)")
+        
+        # Se guarda la nueva ruta en el archivo de configuración
+        with open(ruta_configuracion, "w") as f:
+            f.write(ruta_seleccionada)
+        
+        # Código para actualizar la base de datos
+        
     else:
-        # La configuración existe, se debe verificar si la ruta es la misma
-        with open(ruta_configuracion, "r") as f:
-            ruta_guardada = f.read().strip()
-            
-        if ruta_guardada != ruta_db:
-            # La ruta de la base de datos ha cambiado
-            # Se debe mostrar un mensaje de error o hacer algo al respecto
-            
-            # Se pide al usuario que seleccione la nueva ruta de la base de datos
-            ruta_seleccionada, _ = QFileDialog.getOpenFileName(None, "Seleccionar base de datos", "", "Archivos de base de datos (*.db)")
-            
-            # Se guarda la nueva ruta en el archivo de configuración
-            with open(ruta_configuracion, "w") as f:
-                f.write(ruta_seleccionada)
-            
-            # Código para actualizar la base de datos
-            
-        else:
-            # La ruta de la base de datos es la misma
-            # Se puede continuar con la ejecución del programa
-            pass
+        # La ruta de la base de datos es la misma
+        # Se puede continuar con la ejecución del programa
+        pass
     return ruta_db
-
 
 def conectar_db():
     conn = sqlite3.connect(funcion_de_conexion())
