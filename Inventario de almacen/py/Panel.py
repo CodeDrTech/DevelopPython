35087abcc2d176d4f62620cece543db3d518e91c
CodeDrTech <joseperez8715@gmail.com>
2023-08-22 11:59:50 -0400

    JP

---
+++ Inventario de almacen/py/Panel.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui
+
+class VentanaPrincipal(QMainWindow):    
+    def __init__(self):
+        super().__init__()        
+        uic.loadUi('Inventario de almacen/ui/PANEL.ui',self)
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------        
+                
+        # Configuraiones de la ventana principal.
+        self.setWindowTitle('CONTROL DE INVENTARIO')
+        self.setFixedSize(self.size())
+        self.setWindowIcon(QtGui.QIcon('Inventario de almacen/png/folder.png'))
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------ 
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)       
+    GUI = VentanaPrincipal()
+    GUI.show()
+    sys.exit(app.exec_())
