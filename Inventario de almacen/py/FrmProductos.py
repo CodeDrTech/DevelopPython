dbf62c9877fc3dbcb7ea3b793abd26d38c0945e9
CodeDrTech <joseperez8715@gmail.com>
2023-08-25 09:25:17 -0400

    JP

--- Inventario de almacen/py/FrmProductos.py
+++ Inventario de almacen/py/FrmProductos.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from PyQt5 import QtGui
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 from Consultas_db import insertar_nuevo_producto
 
 class VentanaProductos(QMainWindow):    
@@ -23,8 +23,21 @@ class VentanaProductos(QMainWindow):
         #Llamar a los diferentes formularios desde los botones
         self.btnSalir.clicked.connect(self.fn_Salir)
         self.btnGuardar.clicked.connect(self.insertar_datos)
-        #self.btnLimpiar.clicked.connect(self.abrirFrmFaltanes)
-        #self.btnBorrar.clicked.connect(self.abrirFrmDatosReportes)
+        self.btnLimpiar.clicked.connect(self.visualiza_datos)
+        self.btnBorrar.clicked.connect(self.borrar_fila)
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------ 
+    
+
+    def visualiza_datos(self):
+        # Consulta SELECT * FROM Productos
+        model = QSqlTableModel()
+        model.setTable("Productos")
+        model.select()        
+        self.dataView.setModel(model)
+
+        # Ajustar el tamaño de las columnas para que se ajusten al contenido
+        self.dataView.resizeColumnsToContents()
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------ 
     def insertar_datos(self):
@@ -34,10 +47,41 @@ class VentanaProductos(QMainWindow):
         nombre = self.txtNombre.text()
         medida = self.txtMedida.text()
         insertar_nuevo_producto(codigo, categoria, nombre, medida)
+        self.visualiza_datos()
         
-
-
-
+        
+        #Limpia los TexBox
+        self.txtCodigo.setText("")
+        self.txtCategoria.setText("")
+        self.txtNombre.setText("")
+        self.txtMedida.setText("")
+        self.txtCodigo.setFocus()
+        
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------         
+    def borrar_fila(self):
+        # Obtener el índice de la fila seleccionada
+        indexes = self.dataView.selectedIndexes()
+        
+        if indexes:
+            
+            # Obtener la fila al seleccionar una celda de la tabla
+            index = indexes[0]
+            row = index.row()
+            
+            # Preguntar si el usuario está seguro de eliminar la fila
+            confirmacion = QMessageBox.question(self, "¿ELIMINAR?", "¿ESTAS SEGURO QUE QUIERE ELIMINAR ESTA FILA?",
+                                             QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            
+            
+            # Si el usuario hace clic en el botón "Sí", eliminar la fila
+            if confirmacion == QMessageBox.Yes:
+                # Eliminar la fila seleccionada del modelo de datos
+                model = self.dataView.model()
+                model.removeRow(row)
+                QMessageBox.warning(self, "ELIMINADO", "FILA ELIMINADA.")
+        else:
+            QMessageBox.warning(self, "ERROR", "SELECCIONA LA FILA QUE VAS A ELIMINAR.")
         
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------         
