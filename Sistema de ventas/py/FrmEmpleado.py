2828ebb37f456d52fde59837c65ec6f8a9e1643a
CodeDrTech <joseperez8715@gmail.com>
2023-09-11 17:23:28 -0400

    jp

--- Sistema de ventas/py/FrmEmpleado.py
+++ Sistema de ventas/py/FrmEmpleado.py
@@ -3,15 +3,15 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import QtGui
 
-class VentanaPresentacion(QMainWindow):    
+class VentanaEmpleado(QMainWindow):    
     def __init__(self):
         super().__init__()        
-        uic.loadUi('Sistema de ventas/ui/FrmPresentacion.ui',self)
+        uic.loadUi('Sistema de ventas/ui/FrmEmpleado.ui',self)
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------        
                 
         # Configuraiones de la ventana principal.
-        self.setWindowTitle('.:. Mantenimiento de Presentaciones .:.')
+        self.setWindowTitle('.:. Mantenimiento de Clientes .:.')
         self.setFixedSize(self.size())
         self.setWindowIcon(QtGui.QIcon('Sistema de ventas/png/folder.png'))
 #------------------------------------------------------------------------------------------------------
@@ -19,6 +19,6 @@ class VentanaPresentacion(QMainWindow):
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)       
-    GUI = VentanaPresentacion()
+    GUI = VentanaEmpleado()
     GUI.show()
     sys.exit(app.exec_())
