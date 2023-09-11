807f34255360155e0d9e58594662f1d858c8438b
CodeDrTech <joseperez8715@gmail.com>
2023-09-11 13:00:31 -0400

    JP

---
+++ Sistema de ventas/py/FrmProveedor.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui
+
+class Ventanacategoria(QMainWindow):    
+    def __init__(self):
+        super().__init__()        
+        uic.loadUi('Sistema de ventas/ui/FrmProveedor.ui',self)
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------        
+                
+        # Configuraiones de la ventana principal.
+        self.setWindowTitle('.:. Mantenimiento de Proveedores .:.')
+        self.setFixedSize(self.size())
+        self.setWindowIcon(QtGui.QIcon('Sistema de ventas/png/folder.png'))
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)       
+    GUI = Ventanacategoria()
+    GUI.show()
+    sys.exit(app.exec_())
