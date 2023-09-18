1587cbb29e316105cfa1cbba12b35fa674013ad3
CodeDr <34587303+CodeDrTech@users.noreply.github.com>
2023-09-17 21:31:58 -0400

    jp

--- Sistema de ventas/py/FrmLogin.py
+++ Sistema de ventas/py/FrmLogin.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAbstractItemView
 from PyQt5 import QtGui
+from PyQt5.QtSql import QSqlTableModel, QSqlQuery
 from PyQt5.QtCore import QDateTime
 from FrmPrincipal import VentanaPrincipal
 
@@ -22,24 +23,60 @@ class VentanaLogin(QMainWindow):
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------        
         self.btnSalir.clicked.connect(self.fn_Salir)
+        self.btnIngresar.clicked.connect(self.abrirFrmPrincipal)
         
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------ 
 
+    def visualizar_datos(self):
+        # Consulta SELECT * FROM Productos
+        model = QSqlTableModel()
+        model.setTable("empleado")
+        model.select()        
+        self.tbDatos.setModel(model)
+
+        # Ajustar el tamaño de las columnas para que se ajusten al contenido
+        self.tbDatos.resizeColumnsToContents()    
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------         
+    def obtener_datos_de_fila(self, fila_seleccionada):
+        # Obtener el modelo de datos del QTableView
+        modelo = self.tbDatos.model()
+
+        #if modelo is not None and 0 <= fila_seleccionada < modelo.rowCount():
+        # Obtener los datos de la fila seleccionada
+        columna_9 = modelo.index(fila_seleccionada, 9).data()
+        columna_10 = modelo.index(fila_seleccionada, 10).data()
+        columna_11 = modelo.index(fila_seleccionada, 11).data()
+            
+
+        self.valor_columna_9 = columna_9
+        self.valor_columna_10 = columna_10
+        self.valor_columna_11 = columna_11
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------     
+    def abrirFrmPrincipal(self):
+        self.llamar_venana_principal = VentanaPrincipal()
+        self.llamar_venana_principal.show()
+        self.fn_Salir()
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------     
     def fn_Salir(self):
         self.close()
-       
+        
     def closeEvent(self, event):
         event.accept()
         
     def showEvent(self, event):
         super().showEvent(event)
         
-        self.tbDatos.hide()        
+        self.tbDatos.hide()
+        self.visualizar_datos()        
         fecha_hora = QDateTime.currentDateTime()
         self.txtFecha.setDateTime(fecha_hora)
         self.txtUsuario.setFocus()
-                    
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------                     
 if __name__ == '__main__':
     app = QApplication(sys.argv)       
     GUI = VentanaLogin()
