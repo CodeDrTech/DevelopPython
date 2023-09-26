97f4ad5280cd9514173b45b730d748f9de67c3f1
CodeDrTech <joseperez8715@gmail.com>
2023-09-26 10:00:23 -0400

    Modulo FrmArticulo.py
    Creando las consultas a la base de datos

--- Sistema de ventas/py/FrmArticulo.py
+++ Sistema de ventas/py/FrmArticulo.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAbstractItemView
 from PyQt5 import QtGui
+from PyQt5.QtSql import QSqlTableModel, QSqlQuery
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtSql import QSqlTableModel
+
 
 class VentanaArticulo(QMainWindow):
     ventana_abierta = False     
@@ -17,9 +18,66 @@ class VentanaArticulo(QMainWindow):
         self.setWindowTitle('.:. Mantenimiento de Articulos .:.')
         self.setFixedSize(self.size())
         self.setWindowIcon(QtGui.QIcon('Sistema de ventas/png/folder.png'))
+        
+        
+        # Botones del formulario y sus funciones
+        self.btnGuardar.clicked.connect(self.insertar_datos)
+        #self.btnEditar.clicked.connect(self.editar_datos)
+        self.btnSalir.clicked.connect(self.fn_Salir)
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------
+    def insertar_datos(self):
+        
+        
+        try:
+            codigoventa = self.txtCodVenta.text().upper()
+            nombre = self.txtNombre.text().upper()
+            descripcion = self.txtDescripcion.toPlainText().upper()
+            imagen = self.gFoto
+            categoria = self.cmbCategoria.currentText()
+            presentacion = self.cmbPresentacion.currentText()
+            
+                
+            if  not codigoventa or not nombre or not categoria or not presentacion:
+    
+                mensaje = QMessageBox()
+                mensaje.setIcon(QMessageBox.Critical)
+                mensaje.setWindowTitle("Faltan datos importantes")
+                mensaje.setText("Por favor, complete todos los campos.")
+                mensaje.exec_()
+            
+            
+            else:
+                insertar_nuevo_cliente(codigoventa, nombre, descripcion, imagen, categoria, presentacion)
+        
+                #self.visualiza_datos()
+        
 
+                mensaje = QMessageBox()
+                mensaje.setIcon(QMessageBox.Critical)
+                mensaje.setWindowTitle("Agregar cliente")
+                mensaje.setText("cliente registrado.")
+                mensaje.exec_()
+                
+                
+                #Limpia los TexBox
+                self.txtCodVenta.setText("")
+                self.txtNombre.setText("")
+                self.txtDescripcion.setPlainText("")
+                #self.gFoto
+                self.cmbCategoria.setCurrentText("")
+                self.cmbPresentacion.setCurrentText("")
+                self.txtNombre.setFocus()
+        except Exception as e:
+            # Maneja la excepción aquí, puedes mostrar un mensaje de error o hacer lo que necesites.
+            mensaje = QMessageBox()
+            mensaje.setIcon(QMessageBox.Critical)
+            mensaje.setWindowTitle("Error")
+            mensaje.setText(f"Se produjo un error: {str(e)}")
+            mensaje.exec_()
+            
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------        
     def cargar_categoria(self):    
         # Obtiene los datos de la columna Nombre de la tabla empleados.
         model = QSqlTableModel()
