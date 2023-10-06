2e504abf8a7d47064b65f60a3e6b545401b6f2fd
CodeDrTech <joseperez8715@gmail.com>
2023-10-06 16:47:37 -0400

    jp

--- Sistema de ventas/py/query.py
+++ Sistema de ventas/py/query.py
@@ -1,47 +1,29 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFormLayout, QWidget, QTableView, QVBoxLayout, QFrame, QTabWidget, QGraphicsDropShadowEffect
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QGraphicsColorizeEffect
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QPropertyAnimation
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def colorize_animation(widget):
+    # Creamos un efecto de cambio de color
+    colorize_effect = QGraphicsColorizeEffect()
+    widget.setGraphicsEffect(colorize_effect)
 
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
+    # Creamos una animación para cambiar gradualmente el color
+    animation = QPropertyAnimation(colorize_effect, b"color")
+    animation.setDuration(10000)  # Duración de la animación en milisegundos (2 segundos)
+    animation.setStartValue(QColor(255, 255, 255))  # Color inicial (blanco)
+    animation.setEndValue(QColor(255, 0, 0))        # Color final (rojo)
 
-        layout = QFormLayout(central_widget)
 
-        tab_widget = QTabWidget(self)
-        layout.addWidget(tab_widget)
-
-        # Crear una pestaña en el QTabWidget
-        tab1 = QWidget()
-        tab_widget.addTab(tab1, "Tab 1")
-
-        tab_layout = QVBoxLayout(tab1)
-
-        frame = QFrame(self)
-        frame_layout = QVBoxLayout(frame)
-
-        table_view = QTableView(self)
-        model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(["Columna 1", "Columna 2"])
-        for i in range(10):
-            row = [QStandardItem(f'Dato {i}, 1'), QStandardItem(f'Dato {i}, 2')]
-            model.appendRow(row)
-        table_view.setModel(model)
-
-        # Crear un efecto de sombra y aplicarlo al QTableView
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(10)
-        shadow.setColor(QColor(200, 200, 200))
-        table_view.setGraphicsEffect(shadow)
-
-        frame_layout.addWidget(table_view)
-        tab_layout.addWidget(frame)
+    # Iniciamos la animación
+    animation.start()
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = MainWindow()
+    window = QMainWindow()
+    button = QPushButton("Animar Color")
+    window.setCentralWidget(button)
+
+    button.clicked.connect(lambda: colorize_animation(window))  # Aplicar la animación al hacer clic en el botón
+
     window.show()
     app.exec()
