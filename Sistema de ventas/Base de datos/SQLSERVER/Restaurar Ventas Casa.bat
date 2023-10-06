1f8f7c93d943f409cf87c01e209d6a4ca32bb21a
CodeDrTech <joseperez8715@gmail.com>
2023-10-06 10:11:09 -0400

    jp

--- Sistema de ventas/Base de datos/SQLSERVER/Restaurar Ventas Casa.bat
+++ Sistema de ventas/Base de datos/SQLSERVER/Restaurar Ventas Casa.bat
@@ -1,1 +1,1 @@
-sqlcmd -S localhost -d master -U sa -P Jose.Luis.8715 -Q "RESTORE DATABASE Ventas FROM DISK='C:\Users\Jose\Documents\GitHub\DevelopPython\Sistema de ventas\Base de datos\SQLSERVER\Ventas.bak' WITH REPLACE"
+sqlcmd -S localhost -d master -U sa -P Jose.Luis.8715 -Q "RESTORE DATABASE Ventas FROM DISK='C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\Ventas.bak' WITH REPLACE"
