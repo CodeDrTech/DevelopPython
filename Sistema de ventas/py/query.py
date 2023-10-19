f8397cdd587e57313511c4c7b3ddb2d0dc093eb7
CodeDrTech <joseperez8715@gmail.com>
2023-10-19 10:03:27 -0400

    jp

--- Sistema de ventas/py/query.py
+++ Sistema de ventas/py/query.py
@@ -1,23 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWebEngineWidgets import QWebEngineView  # Importar QWebEngineView
-from PyQt5.QtCore import QUrl  # Importar QUrl desde QtCore
+from PyQt5.QtCore import QUrl
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtWidgets import QApplication
 
-class ReportViewer(QMainWindow):
-    def __init__(self):
-        super().__init__()
+app = QApplication([])
 
-        # Crear un componente WebEngineView para mostrar el informe HTML
-        self.webview = QWebEngineView()
-        self.setCentralWidget(self.webview)
+view = QQuickView()
+view.setSource(QUrl.fromLocalFile('C:/Users/acer/OneDrive/Documentos/UntitledProject/UntitledProject.qml'))
+view.show()
 
-    def show_report(self, html_file):
-        # Cargar el archivo HTML en el WebEngineView
-        self.webview.setUrl(QUrl.fromLocalFile(html_file))
-
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    viewer = ReportViewer()
-    viewer.show_report("informe.html")  # Reemplaza "informe.html" con la ubicación de tu archivo HTML
-    viewer.show()
-    sys.exit(app.exec_())
+app.exec_()
