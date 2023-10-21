054290593be209072ec9d0cca43d99dfc198acee
CodeDr <34587303+CodeDrTech@users.noreply.github.com>
2023-10-20 20:20:20 -0400

    jp

--- Sistema de ventas/py/query.py
+++ Sistema de ventas/py/query.py
@@ -1,11 +1,68 @@
-from PyQt5.QtCore import QUrl
-from PyQt5.QtQuick import QQuickView
-from PyQt5.QtWidgets import QApplication
+import openai  # pip install openai
+import typer  # pip install "typer[all]"
+from rich import print  # pip install rich
+from rich.table import Table
 
-app = QApplication([])
+"""
+Webs de interés:
+- Módulo OpenAI: https://github.com/openai/openai-python
+- Documentación API ChatGPT: https://platform.openai.com/docs/api-reference/chat
+- Typer: https://typer.tiangolo.com
+- Rich: https://rich.readthedocs.io/en/stable/
+"""
 
-view = QQuickView()
-view.setSource(QUrl.fromLocalFile('C:/Users/acer/OneDrive/Documentos/UntitledProject/UntitledProject.qml'))
-view.show()
 
-app.exec_()
+def main():
+
+    openai.api_key = "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"
+
+    print("💬 [bold green]ChatGPT API en Python[/bold green]")
+
+    table = Table("Comando", "Descripción")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear una nueva conversación")
+
+    print(table)
+
+    # Contexto del asistente
+    context = {"role": "system",
+               "content": "Eres un asistente muy útil."}
+    messages = [context]
+
+    while True:
+
+        content = __prompt()
+
+        if content == "new":
+            print("🆕 Nueva conversación creada")
+            messages = [context]
+            content = __prompt()
+
+        messages.append({"role": "user", "content": content})
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages)
+
+        response_content = response.choices[0].message.content
+
+        messages.append({"role": "assistant", "content": response_content})
+
+        print(f"[bold green]> [/bold green] [green]{response_content}[/green]")
+
+
+def __prompt() -> str:
+    prompt = typer.prompt("\n¿Sobre qué quieres hablar? ")
+
+    if prompt == "exit":
+        exit = typer.confirm("✋ ¿Estás seguro?")
+        if exit:
+            print("👋 ¡Hasta luego!")
+            raise typer.Abort()
+
+        return __prompt()
+
+    return prompt
+
+
+if __name__ == "__main__":
+    typer.run(main)
