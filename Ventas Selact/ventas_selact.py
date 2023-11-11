2b219662ca2615fdb46cc3c39db1463d5a9b3a3c
CodeDr <34587303+CodeDrTech@users.noreply.github.com>
2023-11-11 13:24:50 -0400

    jp

--- Ventas Selact/ventas_selact.py
+++ Ventas Selact/ventas_selact.py
@@ -1,8 +1,12 @@
 import os
+import locale
 import openpyxl
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+# Configurar la localización para el formato de moneda
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 # Obtener la ruta completa del archivo Excel
 archivo_excel = os.path.join(os.path.dirname(__file__), 'ventas.xlsx')
@@ -18,8 +22,26 @@ sheet = workbook[hoja_excel]
 
 # Recorrer las filas del archivo Excel
 for fila in sheet.iter_rows(min_row=2, values_only=True):  # Empezamos desde la segunda fila asumiendo que la primera fila contiene encabezados
-    nombre_empleado = fila[0]
-    monto_venta = fila[1]
+    nombre_empleado = str(fila[0]).lower().title()
+
+    # Tratar de convertir el valor de fila[1] a un número flotante
+    if isinstance(fila[1], (int, float)):
+        monto_venta = float(fila[1])
+    elif isinstance(fila[1], str):
+        try:
+            monto_venta = float(fila[1].replace(',', ''))
+        except ValueError as e:
+            print(f"Error: No se puede convertir '{fila[1]}' a un número flotante. Detalles: {e}")
+            continue
+    else:
+        print(f"Error: Tipo de dato no admitido en la columna de montos.")
+        continue
+
+    # Formatear el monto de venta como una cadena con formato de moneda
+    monto_venta_str = locale.currency(monto_venta, grouping=True)
+
+
+
     correo_destinatario = fila[2]
 
     # Configuración del mensaje de correo
