2aea94addc6af09fd97bf056bfa62b23a47584d6
CodeDrTech <joseperez8715@gmail.com>
2023-11-14 11:07:10 -0400

    jp

--- Ventas Selact/ventas_selact3.py
+++ Ventas Selact/ventas_selact3.py
@@ -1,18 +1,16 @@
 import os
 import locale
 import openpyxl
-import base64
 from PyQt5.QtCore import QDate
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
-import matplotlib.pyplot as plt
+from email.mime.application import MIMEApplication
 import io
-from email import encoders
-from email.mime.base import MIMEBase
-from base64 import b64encode
 from PyQt5.QtWidgets import QApplication, QMessageBox
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 
 # Configurar la localización para el formato de moneda
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
@@ -21,99 +19,90 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 archivo_excel = os.path.join(os.path.dirname(__file__), 'Pruebas.xlsx')
 hoja_excel = 'Hoja2'
 
-# Configuración del servidor SMTP de Gmail
-correo_emisor = 'jperez@selactcorp.com'
-contraseña_emisor = 'qsmikukzanvbchro'
-
 # Leer el archivo Excel
 workbook = openpyxl.load_workbook(archivo_excel, data_only=True)
 sheet = workbook[hoja_excel]
 
-# Iniciar la aplicación de PyQt
-app = QApplication([])
-
+# Obtener la fecha actual en formato "MMMM yyyy"
 fecha = QDate.currentDate()
-fecha_formato = fecha.toString("MMMM yyyy")  # Formato mes y año
+fecha_formato = fecha.toString("MMMM yyyy")
 
 # Obtener los datos para el total general
 nombre_total_general = sheet['A28'].value
 monto_total_general = sheet['C28'].value
 
-# Recorrer las filas del archivo Excel
-vendedores = []
-montos = []
+# Crear lista de datos para la tabla en el PDF
+datos_tabla = [['Vendedor', 'Monto Mensual']]
 
-try:
-    for fila in sheet.iter_rows(min_row=3, max_row=27, min_col=1, max_col=4, values_only=True):
-        nombre_empleado = str(fila[0]).lower().title()
-        monto_mensual = fila[2] if fila[2] is not None else 0
-        
-        
-        palabras = nombre_empleado.split()
+# Recorrer las filas del archivo Excel y agregar datos a la lista
+for fila in sheet.iter_rows(min_row=3, max_row=27, min_col=1, max_col=4, values_only=True):
+    nombre_empleado = str(fila[0]).title()
+    monto_mensual = fila[2] if fila[2] is not None else 0
+    datos_tabla.append([nombre_empleado, monto_mensual])
 
-        # Obtener las iniciales de cada palabra
-        iniciales = [palabra[0] for palabra in palabras]
+# Configurar estilos para el PDF
+estilos = getSampleStyleSheet()
 
-        # Unir las iniciales en un solo string
-        iniciales_str = ''.join(iniciales)
-        
-        
+# Configurar la ubicación del archivo PDF
+pdf_path = os.path.join(os.path.dirname(__file__), f'reporte_mensual_{nombre_total_general}.pdf')
 
-        vendedores.append(iniciales_str)
-        montos.append(monto_mensual)
+# Crear el documento PDF
+pdf = SimpleDocTemplate(pdf_path, pagesize=letter)
+contenido = []
 
-        # Verificar si se han recopilado 5 vendedores, luego generar el gráfico y reiniciar las listas
-        if len(vendedores) == 5:
-            # Crear gráfico de barras
-            plt.bar(vendedores, montos, color='blue')
-            plt.xlabel('Vendedores')
-            plt.ylabel('Monto Mensual')
-            plt.title(f'Reporte Mensual - {fecha_formato}')
+# Crear la tabla con los datos
+tabla = Table(datos_tabla)
+tabla.setStyle(TableStyle([
+    ('BACKGROUND', (0, 0), (-1, 0), estilos['Heading1'].backColor),
+    ('TEXTCOLOR', (0, 0), (-1, 0), estilos['Heading1'].textColor),
+    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+    ('FONTNAME', (0, 0), (-1, 0), estilos['Heading1'].fontName),
+    ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+    ('BACKGROUND', (0, 1), (-1, -1), estilos['BodyText'].backColor),
+]))
 
-            # Agregar etiquetas sobre cada barra
-            for i, valor in enumerate(montos):
-                plt.text(i, valor, f'${"{:,.2f}".format(valor)}', ha='center', va='bottom')
+# Agregar la tabla al contenido del PDF
+contenido.append(Paragraph(f'Reporte Mensual - {fecha_formato}', estilos['Heading1']))
+contenido.append(tabla)
 
-            # Guardar la imagen en un archivo
-            imagen_path = f'grafico_{nombre_total_general}.png'
-            plt.savefig(imagen_path)
-            plt.close()
-
-            # Restablecer las listas para el próximo conjunto de vendedores
-            vendedores = []
-            montos = []
-
-            # Configuración del mensaje de correo con el enlace a la imagen
-            asunto = f'Reporte Mensual - {fecha_formato}'
-            cuerpo_mensaje = f'Hola {nombre_total_general},\n\nAdjunto el reporte mensual de los vendedores.\n\n'
-
-            mensaje = MIMEMultipart()
-            mensaje['From'] = f'Notificación de venta <{correo_emisor}>'
-            mensaje['To'] = f'{nombre_total_general} <{sheet["D3"].value}>'
-            mensaje['Subject'] = asunto
-
-            # Adjuntar texto al cuerpo del correo
-            mensaje.attach(MIMEText(cuerpo_mensaje, 'plain'))
-
-            # Adjuntar imagen al cuerpo del correo
-            with open(imagen_path, 'rb') as archivo_imagen:
-                imagen_adjunta = MIMEImage(archivo_imagen.read())
-                imagen_adjunta.add_header('Content-Disposition', 'attachment', filename=os.path.basename(imagen_path))
-                mensaje.attach(imagen_adjunta)
-
-            # Establecer la conexión con el servidor SMTP de Gmail y enviar el correo
-            with smtplib.SMTP('smtp.gmail.com', 587) as server:
-                server.starttls()
-                server.login(correo_emisor, contraseña_emisor)
-                server.sendmail(correo_emisor, mensaje['To'], mensaje.as_string())
-
-            QMessageBox.information(None, 'Correo Enviado', 'El correo se envió correctamente.')
-
-except Exception as e:
-    QMessageBox.critical(None, 'Error al enviar correo', f'Se produjo un error al enviar el correo: {str(e)}')
+# Generar el PDF
+pdf.build(contenido)
 
 # Cerrar el archivo Excel
 workbook.close()
 
-# Salir de la aplicación de PyQt
-app.exec_()
+# Configuración del servidor SMTP de Gmail
+correo_emisor = 'jperez@selactcorp.com'
+contraseña_emisor = 'qsmikukzanvbchro'
+correo_destinatario = 'destinatario@example.com'  # Cambiar al destinatario deseado
+
+# Configurar el mensaje de correo
+asunto = f'Reporte Mensual - {fecha_formato}'
+cuerpo_mensaje = f'Hola {nombre_total_general},\n\nAdjunto el reporte mensual de los vendedores.\n\n'
+
+mensaje = MIMEMultipart()
+mensaje['From'] = f'Notificación de venta <{correo_emisor}>'
+mensaje['To'] = correo_destinatario
+mensaje['Subject'] = asunto
+
+# Adjuntar texto al cuerpo del correo
+mensaje.attach(MIMEText(cuerpo_mensaje, 'plain'))
+
+# Adjuntar archivo PDF al cuerpo del correo
+with open(pdf_path, 'rb') as archivo_pdf:
+    pdf_adjunto = MIMEApplication(archivo_pdf.read(), _subtype="pdf")
+    pdf_adjunto.add_header('Content-Disposition', f'attachment; filename={os.path.basename(pdf_path)}')
+    mensaje.attach(pdf_adjunto)
+
+# Establecer la conexión con el servidor SMTP de Gmail y enviar el correo
+with smtplib.SMTP('smtp.gmail.com', 587) as server:
+    server.starttls()
+    server.login(correo_emisor, contraseña_emisor)
+    server.sendmail(correo_emisor, correo_destinatario, mensaje.as_string())
+
+# Mostrar mensaje informativo
+QMessageBox.information(None, 'Correo Enviado', 'El correo se envió correctamente.')
+
+# Eliminar el archivo PDF después de enviar el correo (opcional)
+os.remove(pdf_path)
+
