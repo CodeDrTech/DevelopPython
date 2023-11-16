d14c001b985d90be213023ea29240f003a2234aa
CodeDrTech <joseperez8715@gmail.com>
2023-11-16 15:04:41 -0400

    jp

--- Ventas Selact/ventas_selact.py
+++ Ventas Selact/ventas_selact.py
@@ -5,13 +5,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from PyQt5.QtWidgets import QApplication, QMessageBox
-#--------------------------------------------------------- Envio de reportes sin meta diaria.
+#--------------------------------------------------------- Envio de reportes sin meta diaria, con monto semanal.
 # Configurar la localización para el formato de moneda
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 # Obtener la ruta completa del archivo Excel
-archivo_excel = os.path.join(os.path.dirname(__file__), 'Pruebas.xlsx')
-hoja_excel = 'Hoja1'
+archivo_excel = os.path.join(os.path.dirname(__file__), 'Ventas.xlsx')
+hoja_excel = 'Envios'
 
 # Configuración del servidor SMTP de Gmail
 correo_emisor = 'jperez@selactcorp.com'
@@ -26,12 +26,12 @@ sheet = workbook[hoja_excel]
 app = QApplication([])
 
 # Recorrer las filas del archivo Excel
-for fila in sheet.iter_rows(min_row=2, max_row=3, min_col=1, max_col=9, values_only=True):
+for fila in sheet.iter_rows(min_row=3, max_row=31, min_col=1, max_col=9, values_only=True):
     nombre_empleado = str(fila[0]).lower().title()
     
-    monto_venta = fila[1] if fila[1] is not None else 0
+    monto_venta_hoy = fila[1] if fila[1] is not None else 0
     
-    esta_Semana = fila[7]
+    monto_venta_semanal = fila[7]
     
     
     
@@ -39,13 +39,13 @@ for fila in sheet.iter_rows(min_row=2, max_row=3, min_col=1, max_col=9, values_o
     correo_destinatario = f'{nombre_empleado} <{str(fila[8]) if fila[8] is not None and fila[8] != "#N/D" else "jperez@selactcorp.com"}>'
 
     
-    # Verificar si el correo_destinatario es válido antes de intentar enviar el correo
+    # Verificar si el correo_destinatario es válido antes de intentar enviar el correo.
     if "@" in correo_destinatario:
         try:
             # Configuración del mensaje de correo
             asunto = 'Información sobre reporte de ventas'
 
-            cuerpo_mensaje = f'Buenas tardes {nombre_empleado}.\n\nTus ventas en el día de hoy suman un total de ${"{:,.2f}".format(monto_venta)}\n\nEsta semana ${"{:,.2f}".format(esta_Semana)}'
+            cuerpo_mensaje = f'Buenas tardes {nombre_empleado}.\n\nTus ventas en el día de hoy suman un total de ${"{:,.2f}".format(monto_venta_hoy)}.\n\nEsta semana ${"{:,.2f}".format(monto_venta_semanal)}.'
 
             mensaje = MIMEMultipart()
             mensaje['From'] = f'Notificacion de reporte <{correo_emisor}>'
