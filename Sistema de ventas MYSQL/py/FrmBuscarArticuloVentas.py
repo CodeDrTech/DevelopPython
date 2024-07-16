2a9ffde73f6269cbe1d4f0d41bf2204036e3b817
CodeDrTech <joseperez8715@gmail.com>
2024-07-16 15:32:55 -0400

    jp

--- Sistema de ventas MYSQL/py/FrmBuscarArticuloVentas.py
+++ Sistema de ventas MYSQL/py/FrmBuscarArticuloVentas.py
@@ -18,7 +18,7 @@ class VentanaBuscarArticuloVentas(QMainWindow):
         # Configuraiones de la ventana principal.
         self.setWindowTitle('.:. Buscar Articulos .:.')
         self.setFixedSize(self.size())
-        self.setWindowIcon(QtGui.QIcon('SSistema de ventas/imagenes/login.jpg'))
+        self.setWindowIcon(QtGui.QIcon('Sistema de ventas/imagenes/login.jpg'))
         
         # Botones del formulario y sus funciones
         self.btnBuscar.clicked.connect(self.buscar_articulo)
