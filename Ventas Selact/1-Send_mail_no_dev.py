9c01811c329763c95b22feb23756a6252834a983
CodeDrTech <joseperez8715@gmail.com>
2024-07-31 09:01:06 -0400

    jp

--- Ventas Selact/1-Send_mail_no_dev.py
+++ Ventas Selact/1-Send_mail_no_dev.py
@@ -15,7 +15,7 @@ hoja_excel = 'Envios'
 
 # Configuración del servidor SMTP de Gmail
 correo_emisor = 'jperez@selactcorp.com'
-contraseña_emisor = 'ggauphuvqfuzjlhm' 
+contraseña_emisor = 'mobfxdnxehuvypqc' 
 
 # Leer el archivo Excel
 workbook = openpyxl.load_workbook(archivo_excel, data_only=True)
