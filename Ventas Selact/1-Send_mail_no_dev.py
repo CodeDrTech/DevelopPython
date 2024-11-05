3eae81aea73dba0a162f22fe976618614072c8e1
CodeDrTech <joseperez8715@gmail.com>
2024-11-05 15:17:08 -0400

    jp

--- Ventas Selact/1-Send_mail_no_dev.py
+++ Ventas Selact/1-Send_mail_no_dev.py
@@ -5,6 +5,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from PyQt5.QtWidgets import QMessageBox
+#idx
 #--------------------------------------------------------- Envio de reportes sin meta diaria.
 # Configurar la localización para el formato de moneda.
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
