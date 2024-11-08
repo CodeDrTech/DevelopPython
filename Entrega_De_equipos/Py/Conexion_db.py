b144615a6ec130185cb932bf60b4b6233f0e1b58
CodeDrTech <joseperez8715@gmail.com>
2024-11-08 17:09:16 -0400

    JP

--- Entrega_De_equipos/Py/Conexion_db.py
+++ Entrega_De_equipos/Py/Conexion_db.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QMessageBox
 
 
 #---------------------------------------------Este modulo esta comentado---------------------------------------------------------
-# Read the configuration file connection_string.txt, which contains the database connection string
+# Read the configuration file connection_string.txt, which contains the database connection string.
 def read_database_config():
     config_path = "Sistema de ventas/txt/connection_string.txt"
     
