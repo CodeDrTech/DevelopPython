c93f90a4b0069e7398b4099298e597650a92bee2
CodeDrTech <joseperez8715@gmail.com>
2024-11-09 08:55:59 -0400

    Eliminar archivos ignorados del repositorio

---
+++ Entrega_De_equipos/Py/FrmArticulo.py
@@ -0,0 +1,169 @@
+import sys
+import io
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAbstractItemView,\
+                        QGraphicsScene, QGraphicsPixmapItem, QGraphicsDropShadowEffect
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtSql import QSqlTableModel, QSqlQuery
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QPixmap
+
+from PIL import Image
+
+#from Consultas_db import insertar_nuevo_articulo, obtener_codigo_articulo, generar_nuevo_codigo_articulo,\
+                            #obtener_ultimo_codigo, generar_nuevo_codigo
+
+
+#---------------------------------------------Este modulo esta comentado---------------------------------------------------------
+class VentanaArticulo(QMainWindow):
+    # Con esta variable se le informa al formulario principal (MDI) 
+    # que este formulario esta abierto y si es llamado no se abra otra vez..
+    ventana_abierta = False
+    
+    def __init__(self):
+        super().__init__()        
+        uic.loadUi('Entrega_De_equipos/Ui/FrmArticulo.ui',self)
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------        
+                
+        # Configuraiones de la ventana principal.
+        self.setWindowTitle('.:. Mantenimiento de Articulos .:.')
+        self.setFixedSize(self.size())
+        self.setWindowIcon(QtGui.QIcon('Sistema de ventas/imagenes/login.jpg'))
+        
+        self.imagen_cargada = None
+        
+        # Botones del formulario y sus funciones.
+        #self.btnGuardar.clicked.connect(self.insertar_datos)
+        #self.btnEditar.clicked.connect(self.editar_datos)
+        #self.btnBuscar.clicked.connect(self.buscar_articulo)
+        self.btnLimpiar.clicked.connect(self.limpiar_imagen)
+        self.btnCargar.clicked.connect(self.cargar_imagen)
+        
+        #self.cmbCategoria.currentTextChanged.connect(self.actualizar_label_categoria)
+        
+        
+        # Crear un efecto de sombra en el tabpage y los groupBox.       
+        tabWidget_shadow = QGraphicsDropShadowEffect()
+        tabWidget_shadow.setBlurRadius(20)
+        tabWidget_shadow.setColor(Qt.black)# type: ignore #QColor(200, 200, 200))        
+        self.tabWidget.setGraphicsEffect(tabWidget_shadow)
+        
+        groupBox_shadow = QGraphicsDropShadowEffect()
+        groupBox_shadow.setBlurRadius(20)
+        groupBox_shadow.setColor(Qt.black)# type: ignore #QColor(200, 200, 200))        
+        self.groupBox.setGraphicsEffect(groupBox_shadow)
+
+        groupBox_2shadow = QGraphicsDropShadowEffect()
+        groupBox_2shadow.setBlurRadius(20)
+        groupBox_2shadow.setColor(Qt.black)# type: ignore #QColor(200, 200, 200))        
+        self.groupBox_2.setGraphicsEffect(groupBox_2shadow)
+
+        # Establecer el texto de referencia a la caja de texto buscar
+        # Conectar el evento de clic para borrar el texto
+        #self.txtBuscar.setPlaceholderText('Buscar')        
+        #self.txtBuscar.mousePressEvent = self.borrarTexto
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+    
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------    
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+    def cargar_imagen(self):
+        # Abre un cuadro de diálogo para seleccionar un archivo de imagen
+        opciones = QFileDialog.Options()
+        opciones |= QFileDialog.ReadOnly  # Asegura que el archivo seleccionado sea solo de lectura
+        archivo_imagen, _ = QFileDialog.getOpenFileName(None, "Seleccionar imagen", "", "Imágenes (*.png *.jpg *.jpeg *.bmp *.gif);;Todos los archivos (*)", options=opciones)
+
+        if archivo_imagen:
+            # Carga la imagen seleccionada usando Pillow (PIL)
+            try:
+                imagen = Image.open(archivo_imagen)
+                # Convierte la imagen en datos binarios
+                imagen_bytes = io.BytesIO()
+                imagen.save(imagen_bytes, format='PNG')  # Puedes ajustar 'PNG' a 'JPEG' u otro formato si es necesario
+                imagen_data = imagen_bytes.getvalue()
+                # Aquí puedes usar "imagen_data" para almacenar o mostrar la imagen según tus necesidades
+                # Por ejemplo, puedes asignarla a un widget de imagen en tu interfaz gráfica
+                
+                # Crea una escena y un elemento gráfico de mapa de bits
+                scene = QGraphicsScene()
+                pixmap = QPixmap()
+                pixmap.loadFromData(imagen_data)
+                pixmap_item = QGraphicsPixmapItem(pixmap)
+            
+                # Agrega el elemento gráfico a la escena
+                scene.addItem(pixmap_item)
+            
+                # Establece la escena en el QGraphicsView
+                self.gFoto.setScene(scene)
+                self.scene = scene
+                # Ajusta la vista para que la imagen ocupe todo el espacio disponible en el QGraphicsView
+                self.gFoto.fitInView(scene.sceneRect(), Qt.KeepAspectRatio)   # type: ignore
+                self.imagen_cargada = imagen_data
+                return imagen_data
+            except Exception as e:
+                # Muestra un mensaje de error utilizando QMessageBox
+                mensaje_error = QMessageBox()
+                mensaje_error.setIcon(QMessageBox.Critical)
+                mensaje_error.setWindowTitle("Error al cargar la imagen")
+                mensaje_error.setText(f"Se produjo un error al cargar la imagen: {str(e)}")
+                mensaje_error.exec_()
+    
+        return None
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------    
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+    #Limpiar la seccion de imaen en casod e que haya alguna.
+    def limpiar_imagen(self):
+        try:
+            self.scene.clear()
+        except Exception as e:
+            mensaje = QMessageBox()
+            mensaje.setIcon(QMessageBox.Critical)
+            mensaje.setWindowTitle("No hay imagen en la caja")
+            mensaje.setText(f"Falta la imagen en: {str(e)}")
+            mensaje.exec_()
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------        
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------          
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------         
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------              
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------          
+    def showEvent(self, event):
+        super().showEvent(event)
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------     
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)       
+    GUI = VentanaArticulo()
+    GUI.show()
+    sys.exit(app.exec_())
