08e548f1e8030cd3238419cf323791a1d35af72e
CodeDrTech <joseperez8715@gmail.com>
2024-11-22 11:16:51 -0400

    actualizacion de ignore

--- Sistema de ventas MYSQL/py/query.py
+++
@@ -1,6 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtSql import QSqlDatabase
-
-app = QApplication([])  # Asegrate de crear una instancia de QApplication antes de QSqlDatabase
-print(QSqlDatabase.drivers())  # Esto imprimirá los drivers disponibles
-
