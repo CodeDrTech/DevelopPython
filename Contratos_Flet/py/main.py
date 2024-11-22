7e469b086bc3efcd48f46d26d266a52058b2d166
CodeDrTech <joseperez8715@gmail.com>
2024-11-22 14:35:14 -0400

    diseñando la interffaz

--- Contratos_Flet/py/main.py
+++ Contratos_Flet/py/main.py
@@ -1,17 +1,61 @@
 import flet as ft
-from frontend.views.usuarios_view import UsuariosView
 
 def main(page: ft.Page):
-    # Configurar propiedades de la ventana
     page.title = "Contratos Flet"
     page.window_width = 800
-    page.window_height = 600
+    page.window.height = 600
     page.scroll = "auto"
-    
-    # Instanciar y cargar la vista principal
-    usuarios_view = UsuariosView()
-    page.add(usuarios_view)
 
-# Iniciar la aplicación
-if __name__ == "__main__":
-    ft.app(target=main)
+    mainTab = ft.Tabs(
+        selected_index=0,  # Pestaña seleccionada por defecto
+        animation_duration=300,
+        expand=True,
+        # contenedor de pestañas
+        tabs=[
+            ft.Tab(
+                icon=ft.icons.FORMAT_LIST_NUMBERED,
+                text="Listado",
+                content=ft.Container(
+                    content=ft.Text("Listado de Contratos", size=20),
+                    padding=20,
+                ),
+            ),
+            ft.Tab(
+                icon=ft.icons.VERIFIED_USER_OUTLINED,
+                text="Datos de Usuario",
+                content=ft.Container(
+                    content=ft.Text("Datos de Usuario", size=20),
+                    padding=20,
+                ),
+            ),
+            ft.Tab(
+                icon=ft.icons.DEVICES,
+                text="Datos del Equipo",
+                content=ft.Container(
+                    content=ft.Text("Datos del Equipo", size=20),
+                    padding=20,
+                ),
+            ),
+            ft.Tab(
+                icon=ft.icons.IMAGE,
+                text="Imágenes",
+                content=ft.Container(
+                    content=ft.Text("Imágenes", size=20),
+                    padding=20,
+                ),
+            ),
+            ft.Tab(
+                icon=ft.icons.LIST,
+                text="Contrato",
+                content=ft.Container(
+                    content=ft.Text("Contrato", size=20),
+                    padding=20,
+                ),
+            ),
+        ],
+    )
+
+    page.add(mainTab)
+    page.update()
+
+ft.app(main)
