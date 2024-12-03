6494ef8f6fd32491acba91c0c18c3ac74ea008ec
CodeDrTech <joseperez8715@gmail.com>
2024-12-03 14:42:07 -0400

    Refactored main.py by removing unused image upload and user registration functionalities, along with associated data handling code.

--- Contratos_Flet/py/main.py
+++ Contratos_Flet/py/main.py
@@ -1,6 +1,5 @@
 import flet as ft
 from database import connect_to_db
-from queries import insertar_nuevo_usuario, insertar_nuevo_equipo, insertar_nuevo_contrato
 from flet import AppView
 import datetime
 
@@ -43,98 +42,10 @@ def main(page: ft.Page):
     page.scroll = "auto" # type: ignore    
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
-    # Función que se ejecuta al seleccionar los archivos
-    def previsualizar_imagenes(e):
-        # Limpiar las imágenes previas
-        imagenes_columna.controls.clear()
-        # Asegurarse de que solo se agreguen hasta 3 imágenes
-        for i, file in enumerate(e.files[:3]):
-            imagen = ft.Image(src=file.path, width=100, height=100)  # Ajustar tamaño de las imágenes
-            imagenes_columna.controls.append(imagen)
-        page.update()
-        
-    # Función para abrir el selector de archivos
-    def abrir_selector_archivos(e):
-        file_picker.pick_files(allow_multiple=True, allowed_extensions=["jpg", "png", "jpeg"]) # Abre el selector de archivos para seleccionar imágenes
 
-    # Contenedor donde se mostrarán las imágenes seleccionadas
-    imagenes_columna = ft.Row()  # Usamos Row para mostrar las imágenes en una fila
-
-    # Crear el FilePicker para seleccionar imágenes
-    file_picker = ft.FilePicker(on_result=previsualizar_imagenes)
-
-    # Añadir el file_picker a la superposición de la página
-    page.overlay.append(file_picker)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
-    # Codigo para insertar datos a la tabla Contrato mediante los controles del tab Contrato
-    # Referencias para los campos de texto del tab Contrato
-    txt_contrato = ft.Ref[ft.TextField]()
-    txt_texto_contrato = ft.Ref[ft.TextField]()
-    txt_id_usuario_contrato = ft.Ref[ft.TextField]()
-    txt_id_equipo_contrato = ft.Ref[ft.TextField]()
-    
-    
-    def agregar_contrato(e):
-        try:
-            numero_Contrato = txt_contrato.current.value
-            texto_Contrato = txt_texto_contrato.current.value
-            id_Usuario = txt_id_usuario_contrato.current.value
-            id_Equipo = txt_id_equipo_contrato.current.value
-            fecha_Contrato = fecha_actual
 
-            if not numero_Contrato or not texto_Contrato or not id_Usuario or not id_Equipo:
-                open_dlg_modal(e)
-            else:
-                # Llama a la función de queries
-                insertar_nuevo_contrato(numero_Contrato, fecha_Contrato, texto_Contrato, id_Usuario, id_Equipo)
-
-                # Muestra un snack_bar al usuario
-                snack_bar = ft.SnackBar(ft.Text("¡Usuario agregado exitosamente!"))
-                page.overlay.append(snack_bar)
-                snack_bar.open = True
-                #page.update() no se activa por estar antes de los dialogos de alerta
-
-
-                # Limpia los campos
-                txt_contrato.current.value = ""
-                txt_texto_contrato.current.value = ""
-                txt_id_usuario_contrato.current.value = ""
-                txt_id_equipo_contrato.current.value = ""
-                
-                page.update()
-
-        except Exception as error:
-            # Muestra un error en snack_bar
-            page.snack_bar = ft.SnackBar(ft.Text(f"Error: {error}"), open=True, duration=3000)
-            page.update()
-    
-    def mostrar_datepicker(e):
-        # Abre el diálogo del DatePicker para que el usuario seleccione una fecha.
-        page.overlay.append(date_picker_dialog)
-        date_picker_dialog.open = True
-        page.update()
-
-    def seleccionar_fecha(e):
-        # Obtiene la fecha seleccionada del DatePicker.
-        fecha_actual = date_picker_dialog.value
-        if fecha_actual:
-            # Convierte a formato solo de fecha sin la hora.
-            fecha_solo = fecha_actual.date()
-            # Actualiza el texto para mostrar la fecha seleccionada.
-            fecha_texto.value = f"               {fecha_solo}"
-            # Cierra el diálogo configurando `open = False`.
-            date_picker_dialog.open = False
-            # Finalmente, actualiza la página para reflejar los cambios.
-            page.update()
-
-    # Inicializa la fecha actual y crea un texto para mostrar la fecha seleccionada.
-    fecha_actual = datetime.date.today()
-    fecha_texto = ft.Text(f"               {fecha_actual}")
-
-    # Crea el DatePicker y establece que `seleccionar_fecha` se ejecutará cuando cambie la fecha seleccionada.
-    date_picker_dialog = ft.DatePicker(
-        on_change=seleccionar_fecha)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------    
     # funciones y control para abrir cuadro de dialogo para avisar al usuario que faltan datos en tab Registrar Usuario.
@@ -159,101 +70,9 @@ def main(page: ft.Page):
     )
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------------------------------------- 
-    # Codigo para insertar datos a la tabla usuario mediante los controles del tab Datos Usuario
-    # Referencias para los campos de texto del tab Datos de Usuario
-    txt_nombre = ft.Ref[ft.TextField]()
-    txt_apellidos = ft.Ref[ft.TextField]()
-    txt_cedula = ft.Ref[ft.TextField]()
-    txt_numero_empleado = ft.Ref[ft.TextField]()
-    def agregar_usuario(e):
-        try:
-            nombre = txt_nombre.current.value
-            apellidos = txt_apellidos.current.value
-            cedula = txt_cedula.current.value
-            numero_empleado = txt_numero_empleado.current.value
 
-            if not nombre or not apellidos or not cedula or not numero_empleado:
-                open_dlg_modal(e)
-
-            else:
-                # Llama a la función de queries
-                insertar_nuevo_usuario(nombre, apellidos, cedula, numero_empleado)
-
-                # Muestra un snack_bar al usuario
-                snack_bar = ft.SnackBar(ft.Text("¡Usuario agregado exitosamente!"))
-                page.overlay.append(snack_bar)
-                snack_bar.open = True
-                page.update()
-
-
-                # Limpia los campos
-                txt_nombre.current.value = ""
-                txt_apellidos.current.value = ""
-                txt_cedula.current.value = ""
-                txt_numero_empleado.current.value = ""
-                page.update()
-
-        except Exception as error:
-            # Muestra un error en snack_bar
-            page.snack_bar = ft.SnackBar(ft.Text(f"Error: {error}"), open=True, duration=3000)
-            page.update()
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
-    # Codigo para insertar datos a la tabla Equipo mediante los controles del tab Datos del Equipo
-    # Referencias para los campos de texto del tab Datos de Equipo
-    txt_id_usuario = ft.Ref[ft.TextField]()
-    txt_marca = ft.Ref[ft.TextField]()
-    txt_modelo = ft.Ref[ft.TextField]()
-    rg_condicion = ft.Ref[ft.RadioGroup]()
-    
-    def agregar_equipo(e):
-        try:
-            
-            # Verificar que la referencia está inicializada
-            if rg_condicion.current is None:
-                raise ValueError("El control RadioGroup no está inicializado.")
-
-            # Obtener el valor seleccionado
-            condicion = rg_condicion.current.value
-            if not condicion:
-                raise ValueError("Debe seleccionar una condición para el equipo.")
-            
-            # Obtener los valores de los campos
-            id_usuario = txt_id_usuario.current.value
-            marca = txt_marca.current.value
-            modelo = txt_modelo.current.value
-            condicion = rg_condicion.current.value  # Obtener el valor del RadioGroup
-
-            if not id_usuario or not marca or not modelo or not condicion:
-                open_dlg_modal(e)
-            else:
-                # Llama a la función de queries para insertar el equipo
-                insertar_nuevo_equipo(id_usuario, marca, modelo, condicion)
-
-
-                # Muestra un snack_bar al usuario
-                snack_bar = ft.SnackBar(ft.Text("¡Equipo agregado exitosamente!"))
-                page.overlay.append(snack_bar)
-                snack_bar.open = True
-                page.update()
-
-                # Limpia los campos
-                txt_id_usuario.current.value = ""
-                txt_marca.current.value = ""
-                txt_modelo.current.value = ""
-                rg_condicion.current.value = None  # Restablece la selección
-                page.update()
-
-        except ValueError as ve:
-            # Manejo específico de validación
-            page.snack_bar = ft.SnackBar(ft.Text(f"Error: {ve}"), open=True, duration=3000)
-            page.update()
-
-        except Exception as error:
-            # Muestra un error general en el snack_bar
-            page.snack_bar = ft.SnackBar(ft.Text(f"Error: {error}"), open=True, duration=3000)
-            page.overlay.append(page.snack_bar)
-            page.update()
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
