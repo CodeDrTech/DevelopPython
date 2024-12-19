cf3a9bf0ad0b07044ea12722e2d404e21d4f8578
CodeDrTech <joseperez8715@gmail.com>
2024-12-19 16:42:52 -0400

    Agrega funcionalidad de selección de fecha
    Implementa un selector de fecha para registrar horas extras.  Permite al usuario seleccionar una fecha a través de un diálogo y la muestra en la interfaz.  También se agregan campos para registrar el código del empleado, nombre, horas trabajadas al 35% y al 100%, destino/comentario, y un botón para registrar la entrada.

--- Horas_extras/py/panel.py
+++ Horas_extras/py/panel.py
@@ -1,5 +1,6 @@
 import flet as ft
 from flet import ScrollMode
+import datetime
 
 
 #Funcion principal para iniciar la ventana con los controles.
@@ -14,6 +15,34 @@ def main(page: ft.Page):
     
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
+    def mostrar_datepicker(e):
+        # Abre el diálogo del DatePicker para que el usuario seleccione una fecha.
+        page.overlay.append(date_picker_dialog)
+        date_picker_dialog.open = True
+        page.update()
+
+    def seleccionar_fecha(e):
+        # Obtiene la fecha seleccionada del DatePicker.
+        fecha_actual = date_picker_dialog.value
+        if fecha_actual:
+            # Convierte a formato solo de fecha sin la hora.
+            fecha_solo = fecha_actual.date()
+            # Actualiza el texto para mostrar la fecha seleccionada.
+            fecha_texto.value = f"               {fecha_solo}"
+            # Cierra el diálogo configurando `open = False`.
+            date_picker_dialog.open = False
+            # Finalmente, actualiza la página para reflejar los cambios.
+            page.update()
+
+    # Inicializa la fecha actual y crea un texto para mostrar la fecha seleccionada.
+    fecha_actual = datetime.date.today()
+    fecha_texto = ft.Text(f"               {fecha_actual}")
+
+    # Crea el DatePicker y establece que `seleccionar_fecha` se ejecutará cuando cambie la fecha seleccionada.
+    date_picker_dialog = ft.DatePicker(
+        on_change=seleccionar_fecha)
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
     mainTab = ft.Tabs(
         selected_index=0,  # Pestaña seleccionada por defecto al iniciar la ventana
         animation_duration=300,
@@ -22,12 +51,32 @@ def main(page: ft.Page):
         # Contenedor de tabs
         tabs=[
             ft.Tab(
-                icon=ft.Icons.HOUSE_ROUNDED,
+                icon=ft.Icons.LOCK_CLOCK,
                 text="Horas",
+                content=ft.Column(
+                    [
+                        ft.Text("Registrar Horas", size=20),
+                        fecha_texto,
+                        ft.ElevatedButton(
+                            text=           "Fecha",
+                            icon=           ft.Icons.CALENDAR_TODAY,
+                            on_click=       mostrar_datepicker,
+                            width=          200
+                        ),
+                        ft.TextField(label="Codigo", width=200, max_length=3, input_filter=ft.InputFilter(allow=True, regex_string=r"^[0-9]*$", replacement_string="")),
+                        ft.Dropdown(label="Nombre", width=200, options=[ft.dropdown.Option("Juan")]),
+                        ft.TextField(label="Hora 35%", width=200, input_filter=ft.InputFilter(allow=True, regex_string=r"^[0-9:]*$", replacement_string="")),
+                        ft.TextField(label="Hora 100%", width=200, input_filter=ft.InputFilter(allow=True, regex_string=r"^[0-9:]*$", replacement_string="")),
+                        ft.TextField(label="Destino/Comentario",width=200),
+                        ft.ElevatedButton(text="Registrar", width=200),
+                    ],
+                    alignment=ft.MainAxisAlignment.START,
+                    spacing=15,
+                ),
             ),
             ft.Tab(
-                icon=ft.Icons.IMAGE,
-                text="Equipos con Imágenes",
+                icon=ft.Icons.PEOPLE,
+                text="Empleados",
             ),
         ],
     )
