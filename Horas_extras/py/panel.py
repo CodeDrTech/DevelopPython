616c13d9e142c21e85b5794888b26c604cca4efd
CodeDrTech <joseperez8715@gmail.com>
2024-12-19 09:37:30 -0400

    Crea la ventana principal de la aplicación
    Implementa la ventana principal de la aplicación utilizando la librería `flet`.  Se configura el tamaño, título, posición y  se evita que sea redimensionable.

---
+++ Horas_extras/py/panel.py
@@ -0,0 +1,27 @@
+import flet as ft
+
+
+#Funcion principal para iniciar la ventana con los controles.
+def main(page: ft.Page):
+    page.title = "Horas Extras"
+    page.window.alignment = ft.alignment.center
+    page.window.width = 1250
+    page.window.height = 800
+    page.window.resizable = False
+    page.padding = 20
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ft.app(main)
